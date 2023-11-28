a033f364f6c7c23bdac667bb27a8f5d8a005111d
Shivamjha12 <Jhasapna544@gmail.com>
2023-11-28 14:50:49 +0530

    adding deploy initials

--- SplitBoxApp/settings.py
+++ SplitBoxApp/settings.py
@@ -121,6 +121,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(STATIC_ROOT, 'static')
+] 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
